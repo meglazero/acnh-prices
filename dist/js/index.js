4bd664dea5591a5bd1b4eb6ab4d98a8f435a6eab
Butt <alexander.vanmeter@gmail.com>
2020-05-09 14:42:40 -0500

    Added sorting features and buttons to sort

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,15 +1,23 @@
 let json_load = ['fish', 'bug', 'misc']
 
 let fish = []
+let fishGen = []
 let bugs = []
+let bugsGen = []
 let misc = []
+let miscGen = []
+fishi = 2
+bugsi = 2
+misci = 2
+fishn = 2
+bugsn = 2
+miscn = 2
 
 // const fishTemplate = document.querySelector('#fishTemplate');
 const fishTable = document.querySelector('#fish')
 const bugsTable = document.querySelector('#bugs')
 const miscTable = document.querySelector('#misc')
 
-console.log('start')
 function loadJSON(callback, table) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -40,24 +48,38 @@ function init(table) {
 function triggered(json, table) {
     if (table == 'fish') {
         fish = json;
-        // console.log(fish)
     } else if (table == 'bug') {
         bugs = json;
-        // console.log(bugs)
     } else if (table == 'misc') {
         misc = json;
-        // console.log(misc)
     }
     if (fish.length > 0 && bugs.length > 0 && misc.length > 0) {
+        // fishGen = fish
+        // bugsGen = bugs
+        // miscGen = misc
         fish.forEach(element => {
-            genFish(element);
+            fishGen.push(element)
         });
         bugs.forEach(element => {
-            genBugs(element)
+            bugsGen.push(element)
         });
         misc.forEach(element => {
+            miscGen.push(element)
+        });
+
+        fishGen.forEach(element => {
+            genFish(element);
+        });
+        bugsGen.forEach(element => {
+            genBugs(element)
+        });
+        miscGen.forEach(element => {
             genMisc(element)
         });
+
+        // fish = fishGen
+        // bugs = bugsGen
+        // misc = miscGen
     }
 };
 
@@ -155,6 +177,219 @@ function genMisc(element) {
     i++;
 
     miscTable.appendChild(miscTemp);
+}
+
+function sortPrice(element) {
+    if (element == 'fish') {
+        const genFishTable = document.querySelectorAll('#fishTab')
+
+        genFishTable.forEach(element => {
+            fishTable.removeChild(element)
+        });
+
+        fishGen.sort(function(a, b) {
+            if (fishi % 2 == 0){
+                return b[2] - a[2];
+            } else {
+                return a[2] - b[2];
+            }
+        })
+
+        fishi % 2 == 0 ? fishi -= 1 : fishi +=1
+
+        fishGen.forEach(element => {
+            genFish(element)
+        });
+    }
+
+    if (element == 'bugs') {
+        const genBugsTable = document.querySelectorAll('#bugsTab')
+
+        genBugsTable.forEach(element => {
+            bugsTable.removeChild(element)
+        });
+
+        bugsGen.sort(function(a, b) {
+            if (bugsi % 2 == 0){
+                return b[2] - a[2];
+            } else {
+                return a[2] - b[2];
+            }
+        })
+
+        bugsi % 2 == 0 ? bugsi -= 1 : bugsi +=1
+
+        bugsGen.forEach(element => {
+            genBugs(element)
+        });
+    }
+
+    if (element == 'misc') {
+        const genMiscTable = document.querySelectorAll('#miscTab')
+
+        genMiscTable.forEach(element => {
+            miscTable.removeChild(element)
+        });
+
+        miscGen.sort(function(a, b) {
+            if (misci % 2 == 0){
+                if(b[3] != undefined && a[3] != undefined){
+                    return b[3] - a[3];
+                } else if (a[3] != undefined) {
+                    return b[2] - a[3];
+                } else if (b[3] != undefined) {
+                    return b[3] - a[2];
+                } else{
+                    return b[2] - a[2]
+                }
+            } else {
+                if(a[3] != undefined && b[3] != undefined){
+                    return a[3] - b[3];
+                } else if (a[3] != undefined) {
+                    return a[3] - b[2];
+                } else if (b[3] != undefined) {
+                    return a[2] - b[3];
+                } else{
+                    return a[2] - b[2]
+                }
+            }
+        })
+
+        misci % 2 == 0 ? misci -= 1 : misci +=1
+
+        miscGen.forEach(element => {
+            genMisc(element)
+        });
+    }
+}
+
+function sortName(element) {
+    if (element == 'fish') {
+        const genFishTable = document.querySelectorAll('#fishTab')
+
+        genFishTable.forEach(element => {
+            fishTable.removeChild(element)
+        });
+
+        fishGen.sort(function(a, b) {
+            if (fishn % 2 == 0){
+                return a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
+            } else {
+                return b[0] < a[0] ? -1 : b[0] > a[0] ? 1 : 0;
+            }
+        })
+
+        fishn % 2 == 0 ? fishn -= 1 : fishn +=1
+
+        fishGen.forEach(element => {
+            genFish(element)
+        });
+    }
+
+    if (element == 'bugs') {
+        const genBugsTable = document.querySelectorAll('#bugsTab')
+
+        genBugsTable.forEach(element => {
+            bugsTable.removeChild(element)
+        });
+
+        bugsGen.sort(function(a, b) {
+            if (bugsn % 2 == 0){
+                return a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
+            } else {
+                return b[0] < a[0] ? -1 : b[0] > a[0] ? 1 : 0;
+            }
+        })
+
+        bugsn % 2 == 0 ? bugsn -= 1 : bugsn +=1
+
+        bugsGen.forEach(element => {
+            genBugs(element)
+        });
+    }
+
+    if (element == 'misc') {
+        const genMiscTable = document.querySelectorAll('#miscTab')
+
+        genMiscTable.forEach(element => {
+            miscTable.removeChild(element)
+        });
+
+        miscGen.sort(function(a, b) {
+            if (miscn % 2 == 0){
+                return a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
+            } else {
+                return b[0] < a[0] ? -1 : b[0] > a[0] ? 1 : 0;
+            }
+        })
+
+        miscn % 2 == 0 ? miscn -= 1 : miscn +=1
+
+        miscGen.forEach(element => {
+            genMisc(element)
+        });
+    }
+}
+
+function clearTable(element) {
+    if (element == 'fish') {
+        const genFishTable = document.querySelectorAll('#fishTab')
+
+        fishGen = []
+        fish.forEach(element => {
+            fishGen.push(element)
+        });
+
+        genFishTable.forEach(element => {
+            fishTable.removeChild(element)
+        });
+        
+        fishGen.forEach(element => {
+            genFish(element) 
+        });
+        fishi = 2
+        fishn = 2
+    }
+
+    if (element == 'bugs') {
+        const genBugsTable = document.querySelectorAll('#bugsTab')
+
+        bugsGen = []
+        bugs.forEach(element => {
+            bugsGen.push(element)
+        });
+
+        genBugsTable.forEach(element => {
+            bugsTable.removeChild(element)
+        });
+        
+        bugsGen.forEach(element => {
+            genBugs(element) 
+        });
+
+        bugsi = 2
+        bugsn = 2
+    }
+
+    if (element == 'misc') {
+        const genMiscTable = document.querySelectorAll('#miscTab')
+
+        miscGen = []
+        misc.forEach(element => {
+            miscGen.push(element)
+        });
+
+        genMiscTable.forEach(element => {
+            miscTable.removeChild(element)
+        });
+        
+        miscGen.forEach(element => {
+            genMisc(element) 
+        });
+        
+        miscn = 2
+        misci = 2
+    }
 }
 
 //fish: name, image, price, location, shadow, time, month1-12
